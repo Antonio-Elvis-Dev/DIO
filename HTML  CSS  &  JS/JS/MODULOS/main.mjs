8fb78c30fde2eb04793a4bdf13bf0ad85ac43078
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2022-07-16 09:16:44 -0300

    teste git

---
+++ HTML  CSS  &  JS/JS/MODULOS/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraHobby, mostraIdade } from "./funcoes.mjs";
+
+console.log(mostraIdade("Elvis", 21))
+console.log(mostraCidade("Elvis","Palmeira dos Índios" ))
+console.log(mostraHobby("Elvis","Dormir" ))
